2c3e1648fc8b2eafc5c6d32db9c366cbfd3c2cb5
psy-code115 <jpenaandrade97@gmail.com>
2025-11-04 18:50:52 -0800

    Initial commit

---
+++ react-portfolio-lab/src/App.jsx
@@ -0,0 +1,46 @@
+// src/App.jsx
+
+const App = () => {
+  const skill = { name: 'lesson', comfort: 8, frontEnd: true, backend: true };
+  const mySkills = [
+  {
+    name:'JavaScript',
+    comfort: 9,
+    frontEnd: true,
+    backend: true
+  },
+  {
+    name:'CSS',
+    comfort: 8,
+    frontEnd: true,
+    backend: false
+  },
+  {
+    name:'HTML',
+    comfort: 10,
+    frontEnd: true,
+    backend: false
+  },
+]
+
+  const skillsList = mySkills.map((skill, index) =>
+    <li lessons={index}>{skill.name}</li>
+  );
+
+  return (
+    <>
+      <h1>My Skills</h1>
+      <ul>
+        {mySkills.map((skill, index) =>
+          <li key={index}>
+            {skill.name} - Comfort: {skill.comfort}
+            {skill.frontEnd ? ' - Frontend' : ''}
+            {skill.backend ? ' - Backend' : ''}
+          </li>
+        )}
+      </ul>
+    </>
+  );
+};
+
+export default App;
